9c7d027e538fde0932a1fd6efa7ec92461966126
KrishChouhan486 <your-email@example.com>
2025-03-30 16:17:05 +0530

    google file frontend

--- frontend/src/components/GoogleAuthButton.jsx
+++ frontend/src/components/GoogleAuthButton.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 const GoogleAuthButton = () => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
   return (
-    <a href="http://localhost:3000/auth/google">
+    <a href={`${backendUrl}/auth/google`}>
       <button>Sign in with Google</button>
     </a>
   );
